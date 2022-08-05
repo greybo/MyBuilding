38beed6efa1048563903a1afc559b39460c299d2
greybo <sergeybotl@gmail.com>
2022-08-05 19:03:18 +0300

    first commit

--- app/src/main/java/com/greybot/mybuilding/MainActivity.kt
+++ app/src/main/java/com/greybot/mybuilding/MainActivity.kt
@@ -1,23 +1,24 @@
 package com.greybot.mybuilding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.Toast
-import com.google.android.material.textfield.TextInputLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.greybot.mybuilding.mask.MaskedEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val inputTextLayout = findViewById<TextInputLayout>(R.id.inputTextLayout)
-        val area = findViewById<EditText>(R.id.area_code)
-        area.setOnClickListener {
-            Toast.makeText(this, "tapped on area", Toast.LENGTH_SHORT).show()
-        }
-        inputTextLayout.error = "Text error"
+        val phoneInput = findViewById<MaskedEditText>(R.id.phone_input)
+        phoneInput.setOnClickListener {
 
-        val inputTextLayout2 = findViewById<TextInputLayout>(R.id.inputTextLayout2)
-        inputTextLayout2.error = "Text error"
+        }
+//        val inputTextLayout = findViewById<TextInputLayout>(R.id.inputTextLayout)
+//        val editText = findViewById<TextInputEditText>(R.id.inputEditText)
+//        editText.setOnClickListener {
+//            Toast.makeText(this, "tapped on area", Toast.LENGTH_SHORT).show()
+//        }
+
+//        val inputTextLayout2 = findViewById<TextInputLayout>(R.id.inputTextLayout2)
+//        inputTextLayout2.error = "Text error"
     }
 }
