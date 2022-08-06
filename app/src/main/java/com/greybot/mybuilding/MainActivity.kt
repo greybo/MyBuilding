0f6ee02548babcbd65c2db44c423df132b4c0a2d
greybo <sergeybotl@gmail.com>
2022-08-06 20:30:55 +0300

    test 4 coroutine actor (is good)

--- app/src/main/java/com/greybot/mybuilding/MainActivity.kt
+++ app/src/main/java/com/greybot/mybuilding/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel.newSingleThreadTest()
+//        viewModel.newSingleThreadTest()
+        viewModel.actorTest()
     }
 }
--- app/src/main/java/com/greybot/mybuilding/base/CoroutinesViewModel.kt
+++ app/src/main/java/com/greybot/mybuilding/base/CoroutinesViewModel.kt
@@ -8,8 +8,8 @@ class CoroutinesViewModel : CompositeViewModel() {
 
     private val log = LogCust2("MainViewModel")
     private var counter = 0
-
     private val singleThreadContext = newSingleThreadContext("Counter")
+    var actorExample = CoroutineActorExample(Dispatchers.Default)
 
     fun newSingleThreadTest() {
         viewModelScope.launch {
@@ -32,4 +32,22 @@ class CoroutinesViewModel : CompositeViewModel() {
         delay(10)
         return 1
     }
+
+
+    fun actorTest() {
+        log.i("actorTest: start")
+        viewModelScope.launch(Dispatchers.IO) {
+            actorExample = CoroutineActorExample(this.coroutineContext)
+            val result = actor(actorExample)
+            log.i("actorTest: = $result")
+        }
+    }
+
+    private suspend fun actor(actorExample: CoroutineActorExample): Int {
+        log.thread("actor")
+        actorExample.add(256)
+        actorExample.remote(56)
+        return actorExample.getCounter()
+    }
+
 }
